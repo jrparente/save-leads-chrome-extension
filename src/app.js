185a8ddc9af313291f3d74fd30869d546406450f
jrparente <jrparente@gmail.com>
2022-10-25 10:16:26 +0100

    Fixed li and a tags, added styling, added manifest.json and an icon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
-
-for (let i=0;i<myLeads.length;i++) {
-    console.log(myLeads[i])
+function displayLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>
+    <a target='_blank' href='https://${myLeads[i]}'>
+    ${myLeads[i]}
+    </a>
+    </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
+
+function addLead() {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  displayLeads();
+}
+
+inputBtn.addEventListener("click", addLead);
