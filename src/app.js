44d0254bffd290dab86c34f70acaeefc13f65d70
jrparente <jrparente@gmail.com>
2022-10-25 11:33:33 +0100

    Added delete button and localStorage

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
 
-function displayLeads() {
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  display(myLeads);
+}
+
+function display(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-    <a target='_blank' href='https://${myLeads[i]}'>
-    ${myLeads[i]}
+    <a target='_blank' href='${leads[i]}'>
+    ${leads[i]}
     </a>
     </li>`;
   }
@@ -18,7 +25,15 @@ function displayLeads() {
 function addLead() {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  displayLeads();
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  display(myLeads);
+}
+
+function deleteAll() {
+  localStorage.clear();
+  myLeads = [];
+  display(myLeads);
 }
 
 inputBtn.addEventListener("click", addLead);
+deleteBtn.addEventListener("dblclick", deleteAll);
