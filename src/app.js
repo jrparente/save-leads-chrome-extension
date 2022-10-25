0a8e443301d0dfd19a4134b2b2d2aa6d049d9a39
jrparente <jrparente@gmail.com>
2022-10-25 12:03:35 +0100

    Added save tab button

--- src/app.js
+++ src/app.js
@@ -4,6 +4,26 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      // and use that tab to fill in out title and url
+      let tab = tabs[0];
+      console.log(tab.url);
+      alert(tab.url);
+    }
+  );
+
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  display(myLeads);
+});
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
